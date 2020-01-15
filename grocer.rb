c826d8f8b3a25614371a6c446b3c2f8228fa7d87
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-15 17:29:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,9 +19,21 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
-    
-  
-  
+  new_array = []
+  index = 0
+  while index < cart.length
+    new_hash = {}
+    new_hash = cart[index]
+    new_hash[:count] = 0
+      inner_index = 0
+      while inner_index < cart.length
+        new_hash[:count] += 1 if new_hash[:name] == cart[:name][inner_index]
+        inner_index += 1
+      end
+      new_array.push(new_hash)
+    index += 1
+  end
+  new_array
 end
 
 def apply_coupons(cart, coupons)
