75bc0a557df0e45476420e09009f81e13106e53e
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-15 16:08:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,7 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   new_hash = {}
-    if collection.include
+    if collection.include?
       new_hash[:item] = name
       matching_hash = new_hash
     else
