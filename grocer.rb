54458b9712fb76f8681271ca334091b907e2562d
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-15 15:53:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   index = 0
   while index < collection.length do
-    match_check = collection[index][:name]
+    match_check = collection[index][:item]
     If name == match_check
       matching_hash = collection[index]
       break
